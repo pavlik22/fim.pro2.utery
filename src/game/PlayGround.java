b19612a9854277e04f4d3ffc36815659321d2542
bilekpa1 <bilekpa1@J08-26.uhk.cz>
2016-11-02 13:07:55 +0100

    trida hrač hotova a začátek hraci plochy

--- src/game/PlayGround.java
+++ src/game/PlayGround.java
@@ -1,6 +1,8 @@
 package game;
 
-public class PlayGround {
+import javax.swing.JPanel;
+
+public class PlayGround extends JPanel{
 	public static final int HEIGHT = 800;
 	public static final int WIDTH = 600;
 	
